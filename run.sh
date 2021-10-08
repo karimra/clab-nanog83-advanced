685db198c429308609873c566326da3c43ef2e9e
Karim Radhouani <medkarimrdi@gmail.com>
2021-10-08 09:17:12 -0700

    update timers

--- run.sh
+++ run.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-# deploy the lab
-sudo clab deploy -t nanog83.clab.yaml --reconfigure
+# deploy the lab.
+# if you have limited compute resources, limit the number of workers by
+# uncommenting the `--max-workers 4` flag
+sudo clab deploy -t nanog83.clab.yaml --reconfigure #--max-workers 4
 
 # get SR Linux Nodes names and join them with a comma
 srl_nodes=$(docker ps -f label=clab-node-kind=srl --format {{.Names}} | paste -s -d, -)
@@ -79,6 +81,8 @@ gnmic --log \
 # watch bgp peers
 while true
 do 
+echo ""
+echo "$(date)"
 gnmic -a clab-nanog83-spine1,clab-nanog83-spine2,clab-nanog83-spine3,clab-nanog83-spine4 \
       --skip-verify \
       -u admin \
