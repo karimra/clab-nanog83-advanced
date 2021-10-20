1f4409029b0900c55118bc1394c6fe6d6ffe7b71
Karim Radhouani <medkarimrdi@gmail.com>
2021-10-19 17:52:38 -0700

    update config templates

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+echo "gnmic version: $(gnmic version | grep version)"
+echo "clab version: $(sudo containerlab version | grep version)"
 # deploy the lab.
 # if you have limited compute resources, limit the number of workers by
 # uncommenting the `--max-workers 4` flag
@@ -20,20 +22,10 @@ gnmic --log \
       -e json_ietf \
       set \
       --request-file config/1.interfaces/interfaces_template.gotmpl \
-      --request-vars config/1.interfaces/interfaces_template_vars.yaml
-
-# configure subinterfaces via gNMI 
-gnmic --log \
-      -a ${srl_nodes} \
-      --tls-ca clab-nanog83/ca/root/root-ca.pem \
-      -u admin \
-      -p admin \
-      -e json_ietf \
-      set \
       --request-file config/1.interfaces/subinterfaces_template.gotmpl \
       --request-vars config/1.interfaces/interfaces_template_vars.yaml
 
-# configure routing policy prefix set via gNMI 
+# configure routing policy via gNMI 
 gnmic --log \
       -a ${srl_nodes} \
       --tls-ca clab-nanog83/ca/root/root-ca.pem \
@@ -42,21 +34,11 @@ gnmic --log \
       -e json_ietf \
       set \
       --request-file config/2.routing-policy/routing_policy_prefix_set_template.gotmpl \
-      --request-vars config/2.routing-policy/routing_policy_vars.yaml
-
-# configure routing policy policy via gNMI 
-gnmic --log \
-      -a ${srl_nodes} \
-      --tls-ca clab-nanog83/ca/root/root-ca.pem \
-      -u admin \
-      -p admin \
-      -e json_ietf \
-      set \
       --request-file config/2.routing-policy/routing_policy_policy_template.gotmpl \
       --request-vars config/2.routing-policy/routing_policy_vars.yaml
 
 
-# configure network-instance via gNMI 
+# # configure network-instance via gNMI 
 gnmic --log \
       -a ${srl_nodes} \
       --tls-ca clab-nanog83/ca/root/root-ca.pem \
@@ -65,35 +47,27 @@ gnmic --log \
       -e json_ietf \
       set \
       --request-file config/3.network-instance/network_instance_template.gotmpl \
-      --request-vars config/3.network-instance/network_instance_template_vars.yaml
-
-# configure bgp via gNMI 
-gnmic --log \
-      -a ${srl_nodes} \
-      --tls-ca clab-nanog83/ca/root/root-ca.pem \
-      -u admin \
-      -p admin \
-      -e json_ietf \
-      set \
       --request-file config/3.network-instance/network_instance_bgp_template.gotmpl \
       --request-vars config/3.network-instance/network_instance_template_vars.yaml
 
+echo "$(date): Waiting 5s before checking nodes BGP status"
 sleep 5
 
-for node in $(docker ps -f label=clab-node-kind=srl --format {{.Names}})
+for node in $(docker ps -f label=clab-node-kind=srl -f label=containerlab=nanog83 --format {{.Names}})
 do 
-  bgp_state=$(gnmic -a $node --tls-ca clab-nanog83/ca/root/root-ca.pem -u admin -p admin -e ascii --format flat get --path /network-instance[name=default]/protocols/bgp/oper-state | awk '{print $NF}')
+  bgp_state=$(gnmic-dev -a $node --tls-ca clab-nanog83/ca/root/root-ca.pem -u admin -p admin -e ascii --format flat get --path /network-instance[name=default]/protocols/bgp/oper-state | awk '{print $NF}')
   echo "$(date): BGP state of $node is $bgp_state"
   if [ $bgp_state != "up" ]; then 
     exit 1
   fi
 done
 # watch bgp peers
+
 while true
 do 
 echo ""
 echo "$(date)"
-gnmic -a clab-nanog83-spine11,clab-nanog83-spine12,clab-nanog83-spine21,clab-nanog83-spine22 \
+gnmic-dev -a clab-nanog83-spine11,clab-nanog83-spine12,clab-nanog83-spine21,clab-nanog83-spine22 \
       --tls-ca clab-nanog83/ca/root/root-ca.pem \
       -u admin \
       -p admin \
