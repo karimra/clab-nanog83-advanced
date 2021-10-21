ecf6b8d2cfdd2dc3d51c1b0ed9f2bc2f84a78586
Karim Radhouani <medkarimrdi@gmail.com>
2021-10-20 22:33:17 -0700

    add bgp neighbors check

--- run.sh
+++ run.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-echo "gnmic version : $(gnmic version | grep version | awk '{print $NF}')"
-echo "clab version  : $(sudo containerlab version | grep version | awk '{print $NF}')"
+gnmic_cmd="gnmic --tls-ca clab-nanog83/ca/root/root-ca.pem -u admin -p admin"
+
+printf "gnmic version : $(gnmic version | grep version | awk '{print $NF}')\n"
+printf "clab version  : $(sudo containerlab version | grep version | awk '{print $NF}')\n"
+
 # deploy the lab.
 # if you have limited compute resources, limit the number of workers by
 # uncommenting the `--max-workers 4` flag
@@ -11,14 +14,11 @@ sudo clab deploy -t nanog83.clab.yaml --reconfigure #--max-workers 4
 
 # get SR Linux Nodes names and join them with a comma
 srl_nodes=$(docker ps -f label=containerlab=nanog83 -f label=clab-node-kind=srl --format {{.Names}} | paste -s -d, -)
-echo $srl_nodes
 
 # configure interfaces via gNMI 
-gnmic --log \
+${gnmic_cmd} \
+      --log \
       -a ${srl_nodes} \
-      --tls-ca clab-nanog83/ca/root/root-ca.pem \
-      -u admin \
-      -p admin \
       -e json_ietf \
       set \
       --request-file config/1.interfaces/interfaces_template.gotmpl \
@@ -26,11 +26,9 @@ gnmic --log \
       --request-vars config/1.interfaces/interfaces_template_vars.yaml
 
 # configure routing policy via gNMI 
-gnmic --log \
+${gnmic_cmd} \
+      --log \
       -a ${srl_nodes} \
-      --tls-ca clab-nanog83/ca/root/root-ca.pem \
-      -u admin \
-      -p admin \
       -e json_ietf \
       set \
       --request-file config/2.routing-policy/routing_policy_prefix_set_template.gotmpl \
@@ -39,42 +37,56 @@ gnmic --log \
 
 
 # # configure network-instance via gNMI 
-gnmic --log \
+${gnmic_cmd} \
+      --log \
       -a ${srl_nodes} \
-      --tls-ca clab-nanog83/ca/root/root-ca.pem \
-      -u admin \
-      -p admin \
       -e json_ietf \
       set \
       --request-file config/3.network-instance/network_instance_template.gotmpl \
       --request-file config/3.network-instance/network_instance_bgp_template.gotmpl \
       --request-vars config/3.network-instance/network_instance_template_vars.yaml
 
-echo "$(date): Waiting 5s before checking nodes BGP status"
+printf "\n"
+printf "$(date): Waiting 5s before checking nodes BGP status\n"
 sleep 5
 
 for node in $(docker ps -f label=clab-node-kind=srl -f label=containerlab=nanog83 --format {{.Names}})
 do 
-  bgp_state=$(gnmic-dev -a $node --tls-ca clab-nanog83/ca/root/root-ca.pem -u admin -p admin -e ascii --format flat get --path /network-instance[name=default]/protocols/bgp/oper-state | awk '{print $NF}')
-  echo "$(date): BGP state of $node is ${bgp_state^^}"
-  if [ $bgp_state != "up" ]; then 
+  bgp_state=$(${gnmic_cmd} \
+                  -a $node \
+                  -e ascii \
+                  --format flat \
+                  get \
+                  --path /network-instance[name=default]/protocols/bgp/oper-state | awk '{print $NF}')
+  
+  
+  if [ $bgp_state != "up" ]; then
+    printf "unexpected BGP state for node $node : ${bgp_state^^} (ノ ゜Д゜)ノ ︵ ┻━┻\n"
     exit 1
   fi
+  printf "$(date): \\U1F44D BGP state of $node is ${bgp_state^^} \n"
 done
-# watch bgp peers
 
-while true
-do 
-echo ""
-echo "$(date)"
-gnmic-dev -a clab-nanog83-spine11,clab-nanog83-spine12,clab-nanog83-spine21,clab-nanog83-spine22 \
-      --tls-ca clab-nanog83/ca/root/root-ca.pem \
-      -u admin \
-      -p admin \
-      -e ascii \
-      --format flat \
-      get \
-      --path /network-instance[name=default]/protocols/bgp/oper-state \
-      --path /network-instance[name=default]/protocols/bgp/neighbor/ipv4-unicast/oper-state
-sleep 10
+printf "\n"
+printf "$(date): Waiting 15s for BGP sessions to be established\n"
+sleep 15
+
+for node in $(docker ps -f label=clab-node-kind=srl -f label=containerlab=nanog83 -f label=is_spine=true --format {{.Names}})
+do
+num_neighbors=$(
+      gnmic -a $node \
+            --tls-ca clab-nanog83/ca/root/root-ca.pem \
+            -u admin \
+            -p admin \
+            -e ascii \
+            --format flat \
+            get \
+            --path /network-instance[name=default]/protocols/bgp/neighbor/ipv4-unicast/oper-state | grep neighbor | wc -l
+            )
+      if [ "$num_neighbors" -ne "6" ]
+      then
+        printf "unexpected number of neighbors for node $node : $num_neighbors (ノ ゜Д゜)ノ ︵ ┻━┻\n"
+        exit 1
+      fi
+      printf "$(date): \\U1F44D $node has $num_neighbors BGP neighbors \n"
 done
